37b085803cc6092c468495937cde05e2d66c1c7e
lazil <l89155996021@gmail.com>
2023-03-16 21:31:24 +0300

    task 1

---
+++ src/main/java/ExpensesManager.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class ExpensesManager {
+    double[] expenses;
+    // Scanner scanner =  new Scanner(System.in);
+    ExpensesManager(){
+        expenses = new double[7];
+    }
+    double saveExpense(double moneyBeforeSalary, double expense, int day) {
+
+        // Печать вопросов и считывание ответов оставьте в классе Praktikum
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses[day - 1] = expenses[day - 1] + expense;
+        System.out.println("Значение сохранено! Ваш текущий баланс в рублях: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 1000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    void printAllExpenses() {
+        for (int i = 0; i < expenses.length; i++) {
+            System.out.println("День " + (i + 1) + ". Потрачено " + expenses[i] + " рублей");
+        }
+    }
+    double findMaxExpense() {
+        double maxExpense = 0;
+        for (int i = 0; i < expenses.length; i++) {
+            if (expenses[i] > maxExpense) {
+                maxExpense = expenses[i];
+            }
+        }
+        return maxExpense;
+    }
+}
